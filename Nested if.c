82c01e41eec64255be7a91435a09440c84745d38
Dhruvdevsingh <147177787+Dhruvdevsingh@users.noreply.github.com>
2024-01-05 21:53:36 +0530

    Nested if.c

--- Nested if.c
+++ Nested if.c
@@ -1,17 +1,22 @@
-#include <stdio.h>
-
-int main() {
-
-  double n1, n2, n3;
-
-  printf("Enter three different numbers: ");
-  scanf("%lf %lf %lf", &n1, &n2, &n3);
-  if (n1 >= n2 && n1 >= n3)
-    printf("%.2f is the largest number.", n1);
-  if (n2 >= n1 && n2 >= n3)
-    printf("%.2f is the largest number.", n2);
-  if (n3 >= n1 && n3 >= n2)
-    printf("%.2f is the largest number.", n3);
-
-  return 0;
+// Program for finding the largest of three numbers using nested if
+#include<stdio.h>
+void main()
+{
+    int A,B,C;
+    printf("Enter three numbers=");
+    scanf("%d%d%d",&A,&B,&C);
+    if(A>B)
+    {
+        if(A>C)
+        printf("%d is the greatest number",A);
+        else
+        printf("%d is the grestest numbrer",C);
+    }
+    else
+    {
+     if(B>C)
+     printf("%d is the greatest number",B);
+     else
+     printf("%d is the greatest number",C);
+    }
 }
